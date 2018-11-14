983eb75adf9bfc3aea48b5dc9eae941a00d0fd8b
Artur Schulz <artur.schulz123@gmail.com>
2018-11-14 15:08:26 +0100

    add profile page

--- admin/profile.php
+++ admin/profile.php
@@ -1,27 +1,29 @@
+<?php include "includes/admin_header.php" ?>
+
+
 <?php
+    if(isset($_SESSION['username'])) {
+        $username = $_SESSION['username'];
+        $query = "SELECT * FROM users WHERE user_name = '{$username}' ";
+        $select_user_profile_query = mysqli_query($connect, $query);
+        while ($row = mysqli_fetch_array($select_user_profile_query)) {
+            $user_id = $row['user_id'];
+            $username = $row['user_name'];
+            $user_password = $row['user_password'];
+            $user_firstname = $row['user_firstname'];
+            $user_lastname = $row['user_lastname'];
+            $user_email = $row['user_email'];
+            $user_image = $row['user_image'];
+            $user_role = $row['user_role'];
+            $user_randSalt = $row['user_randSalt'];
+        }
+    }
 
-if(isset($_GET['edit_user'])) {
-$the_user_id = $_GET['edit_user'];
+?>
 
-
-    $query = "SELECT * FROM users WHERE user_id = $the_user_id "; //find categories
-    $select_users_query = mysqli_query($connect, $query);
-
-    while ($row = mysqli_fetch_assoc($select_users_query)) {
-    $user_id = $row['user_id'];
-    $username = $row['user_name'];
-    $user_password = $row['user_password'];
-    $user_firstname = $row['user_firstname'];
-    $user_lastname = $row['user_lastname'];
-    $user_email = $row['user_email'];
-    $user_image = $row['user_image'];
-    $user_role = $row['user_role'];
-    $user_randSalt = $row['user_randSalt']; 
-}
-
-}
-
-    if(isset($_POST['edit_user'])) {
+<?php
+    
+     if(isset($_POST['edit_user'])) {
             $user_firstname = $_POST['user_firstname'];
             $user_lastname = $_POST['user_lastname'];
             $user_role = $_POST['user_role'];
@@ -43,14 +45,33 @@ $the_user_id = $_GET['edit_user'];
             $query .= "user_name = '{$username}', ";
             $query .= "user_email = '{$user_email}', ";
             $query .= "user_password   = '{$user_password }'  ";
-            $query .= "WHERE user_id = {$the_user_id} ";    
+            $query .= "WHERE username = '{$username}'' ";    
             $edit_user_query = mysqli_query($connect, $query);
             confirmQuery($edit_user_query);
 
-        }
+        }  
+
+
+
 ?>
 
+    <div id="wrapper">
 
+        <!-- Navigation -->
+        <?php include "includes/admin_navigation.php" ?>
+
+
+        <div id="page-wrapper">
+
+            <div class="container-fluid">
+
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">
+                            Welcome to Admin Panel
+                            <small>Subheading</small>
+                        </h1>
 
 <form action="" method="post" enctype="multipart/form-data">
     <div class="form-group">
@@ -67,7 +88,7 @@ $the_user_id = $_GET['edit_user'];
 
         <select name="user_role" id="">
 
-<option value="subscriber"><?php echo $user_role; ?></option>
+        <option value="subscriber"><?php echo $user_role; ?></option>
             <?php
                 if($user_role == 'admin') {
                    echo  "<option value='subscriber'>subscriber</option>";
@@ -105,11 +126,19 @@ $the_user_id = $_GET['edit_user'];
 
 
     <div class="form-group">
-        <input type="submit" class="btn btn-primary" name="edit_user" value="Edit User">
+        <input type="submit" class="btn btn-primary" name="edit_user" value="Update Profile">
     </div>
 
-
-
-
-
 </form>
+                    
+                    </div>
+                </div>
+                <!-- /.row -->
+
+            </div>
+            <!-- /.container-fluid -->
+
+        </div>
+        <!-- /#page-wrapper -->
+        <?php include "includes/admin_footer.php" ?>
+
